8a6f451ef57c2510f0abaa8363bb0c50ae502cc0
Mike Blume <mike@loggly.com>
2013-08-06 10:09:16 -0700

    add repo for core.async

--- project.clj
+++ project.clj
@@ -9,6 +9,7 @@
                  [hiccup "1.0.4"]]
   :plugins [[lein-cljsbuild "0.3.2"]
             [lein-ring "0.8.6"]]
+  :repositories {"sonatype-oss-public" "https://sonatype.org/content/groups/public"}
   :cljsbuild {
     :builds [{:source-paths ["src/cljs"]
               :compiler {:output-to "resources/public/js/main.js"
