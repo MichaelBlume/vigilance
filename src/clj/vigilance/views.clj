6175cb68473378362159a027745855bb15f1e346
Mike Blume <mike@loggly.com>
2013-08-06 19:22:17 -0700

    make js link relative

--- src/clj/vigilance/views.clj
+++ src/clj/vigilance/views.clj
@@ -9,6 +9,6 @@
     [:head
       [:title "Hello World"]
       (include-js "//ajax.googleapis.com/ajax/libs/jquery/1.9.0/jquery.min.js")
-      (include-js "/js/main.js")]
+      (include-js "js/main.js")]
     [:body
       [:h1 "Hello World"]]))
